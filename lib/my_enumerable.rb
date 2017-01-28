ff209011ad3546cb5116d05886215d1baf68273a
Mark R <mwritchie2@gmail.com>
2017-01-27 19:48:44 -0600

    completed TOP - Advanced Ruby Building Blocks

--- lib/my_enumerable.rb
+++ lib/my_enumerable.rb
@@ -43,4 +43,57 @@ module Enumerable
       self.my_select {|s| !s.nil?} == self ? true : false
     end
   end
+
+  def my_any?
+    if block_given?
+      self.my_each {|s| if (yield s) then return true end}
+    else
+      self.my_each {|s| if !(s.nil?) then return true end}
+    end
+  end
+
+  def my_none?
+    if block_given?
+      self.my_each {|s| if (yield s) then return false end}
+      true
+    else
+      self.my_each {|s| if !(s.nil?) then return false end}
+      true
+    end
+  end
+
+  def my_count
+    counted = 0
+    if block_given?
+      counted = self.my_select { |s| yield s }.length
+    else
+      counted = self.my_select { |s| !s.nil? }.length
+    end
+  end
+
+  def my_map(*procs)
+    if procs[0].is_a?(Proc)
+      result = []
+      self.my_each { |s| result.push procs[0].call s}
+      result
+    elsif block_given?
+      result = []
+      self.my_each { |s| result.push yield s}
+      result
+    else
+      self.to_enum
+    end
+  end
+
+  def my_inject(*initial, &binary)
+    initial.empty? ? result = self[0] : result = initial[0]
+    (self.size - 1).times do |s|
+      result = yield result,self[s + 1]
+    end
+    result
+  end
+
+  def self.multiply_els(array)
+    array.my_inject { |prod,s| prod*s }
+  end
 end
--- spec/my_enumerable_spec.rb
+++ spec/my_enumerable_spec.rb
@@ -4,6 +4,10 @@ def array
   [1,2,3]
 end
 
+some_proc = Proc.new do |pee|
+  pee**pee
+end
+
 describe "#my_each" do
   context "when called alone" do
     it "returns an enumerator"  do
@@ -47,9 +51,63 @@ end
 
 describe "#my_all?" do
   context "when called on a collection" do
-    it "verifies conformity to the  given block"  do
+    it "verifies every element conforms to the  given block"  do
       expect(array.my_all?).to be(true)
       expect(array.my_all? {|s| s % 2 == 0}).to be(false)
+    end
+  end
+end
+
+describe "#my_any?" do
+  context "when called on a collection" do
+    it "verifies at least one element conforms to the given block"  do
+      expect(array.my_any?).to be(true)
+      expect(array.my_any? {|a| a % 2 == 0}).to be(true)
+    end
+  end
+end
+
+describe "#my_none?" do
+  context "when called on a collection" do
+    it "verifies none of the elements conform to the given block"  do
+      expect(array.my_none?).to be(false)
+      expect(array.my_none? {|a| a > 3}).to be(true)
+    end
+  end
+end
+
+describe "#my_count" do
+  context "when called on a collection" do
+    it "returns the number of elements that return true"  do
+      expect(array.my_count).to eq(3)
+      expect(array.my_count {|a| a % 2 == 0}).to eq(1)
+    end
+  end
+end
+
+describe '#my_map' do
+  context 'when called on a collection' do
+    it 'returns a new collection with the results of the given block' do
+      expect(array.my_map).to be_an(Enumerator)
+      expect(array.my_map { |a| a**a }).to eq([1,4,27])
+      expect(array.my_map some_proc).to eq([1,4,27])
+    end
+  end
+end
+
+describe '#my_inject' do
+  context 'when called on a collection' do
+    it 'combines all elements based on a given binary operation' do
+      expect(array.my_inject { |sum,abj| sum + abj }).to eq(6)
+      expect(array.my_inject { |dif,a| dif - a}).to eq(-4)
+    end
+  end
+end
+
+describe "#multiply_els" do
+  context "when called with an array as argument" do
+    it "uses #my_inject to find the product of the members"  do
+      expect(Enumerable.multiply_els([1,2,3,4])).to eq(24)
     end
   end
 end
